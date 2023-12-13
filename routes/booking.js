fdd9a74c5bf8e7f32697d8cf561041ec90114f80
sara-husain <sarahusain2428@gmail.com>
2023-12-13 18:40:12 +0300

    booking is DONE

--- routes/booking.js
+++ routes/booking.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router =  express.Router();
 const bookingCntrl = require("../controllers/booking");
-// const isLoggedIn = require('../config/isLoggedIn');
+const isLoggedIn = require('../config/isLoggedIn');
 
 //routes
-router.get("/check",   bookingCntrl.booking_check_create_get);
-router.post("/check",   bookingCntrl.booking_check_create_post);
-router.get("/add",   bookingCntrl.booking_create_get);
-router.post("/add",   bookingCntrl.booking_create_post);
-router.get("/index",   bookingCntrl.booking_index_get);
-router.get("/detail",   bookingCntrl.booking_show_get);
-router.get("/delete",   bookingCntrl.booking_delete_get);
-router.get("/edit",   bookingCntrl.booking_edit_get);
-router.post("/update",   bookingCntrl.booking_update_post);
+router.get("/check", isLoggedIn, bookingCntrl.booking_check_create_get);
+router.post("/check", isLoggedIn,  bookingCntrl.booking_check_create_post);
+router.get("/add", isLoggedIn,  bookingCntrl.booking_create_get);
+router.post("/add", isLoggedIn,  bookingCntrl.booking_create_post);
+router.get("/index", isLoggedIn,  bookingCntrl.booking_index_get);
+router.get("/detail", isLoggedIn,  bookingCntrl.booking_show_get);
+router.get("/delete", isLoggedIn,  bookingCntrl.booking_delete_get);
+router.get("/edit", isLoggedIn,  bookingCntrl.booking_edit_get);
+router.post("/update", isLoggedIn,  bookingCntrl.booking_update_post);
 
 module.exports = router;
